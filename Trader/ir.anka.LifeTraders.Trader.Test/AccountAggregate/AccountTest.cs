896c7a3a79863a694f24b2924086983a79dfacef
FarazLoloei <FarazLoloei@users.noreply.github.com>
2023-10-02 14:43:52 +0330

    Exception message template is added to Default Data

---
+++ Trader/ir.anka.LifeTraders.Trader.Test/AccountAggregate/AccountTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using ir.anka.LifeTraders.Trader.Core.Domain.AccountAggregate;
+using ir.anka.LifeTraders.Trader.Core.Domain.AccountAggregate.Exceptions;
+using static ir.anka.LifeTraders.Common.Infrastructure.DefaultData.Exceptions;
+
+namespace ir.anka.LifeTraders.Trader.Test.AccountAggregate;
+
+public class AccountTest : AccountTestProvider
+{
+    [Theory]
+    [ClassData(typeof(AccountTestCases))]
+    public void TestCurrencyConstructor(string username, string password, Guid brokerId)
+    {
+        var account = new Account(username, password, brokerId, accountValidator.Object);
+
+        AssertAccountConstructorTest(username, password, brokerId, account);
+    }
+
+    [Theory]
+    [ClassData(typeof(EmptyNullAccountTestCases))]
+    public void TestAccountValidator(string username, string password, Guid brokerId)
+    {
+        Action action = () => new Account(username, password, brokerId, accountValidator.Object);
+        action.Should().Throw<AccountValidateException>()
+                    .WithMessage(string.Format(EXCEPTION_MESSAGE_TEMPLATE, "account"));
+    }
+
+    private void AssertAccountConstructorTest(string username, string password, Guid brokerId, Account expectedAccount)
+    {
+        expectedAccount.Should().NotBeNull();
+
+        username.Should().Be(expectedAccount.Username);
+        password.Should().Be(expectedAccount.Password);
+        brokerId.Should().Be(expectedAccount.BrokerId);
+    }
+
+
+}
---
+++ Trader/ir.anka.LifeTraders.Trader.Test/AccountAggregate/AccountTestCases.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace ir.anka.LifeTraders.Trader.Test.AccountAggregate;
+
+internal sealed class AccountTestCases : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "2123362", "cQ8imzbU", Guid.Parse("2a7b8f2cb106452a9f904ece18730268") };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+internal sealed class EmptyNullAccountTestCases : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { "", "", Guid.Empty };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
