f2b65d3a6b32d38514402ada20ce8403f34b3c12
FarazLoloei <FarazLoloei@users.noreply.github.com>
2023-10-08 16:28:46 +0330

    Plan Convertor Test

---
+++ Fund/ir.anka.LifeTraders.Fund.Core/Domain/PlanAggregate/Converters/PlanConverter.cs
@@ -0,0 +1,49 @@
+﻿using ir.anka.LifeTraders.Fund.Core.Domain.PlanAggregate.Abstraction;
+using ir.anka.LifeTraders.Fund.Core.Domain.PlanAggregate.DTOs;
+
+namespace ir.anka.LifeTraders.Fund.Core.Domain.PlanAggregate.Converters;
+
+public class PlanConverter : IPlanConverter
+{
+    public PlanDto PlanToPlanDtoConverter(Plan record)
+    {
+        return CreatePlanDto(record);
+    }
+
+    public async Task<PlanDto> PlanToPlanDtoConverterAsync(Plan record)
+    {
+        var task = await Task.Run(() =>
+         {
+             return CreatePlanDto(record);
+         });
+
+        return task;
+    }
+
+    private static PlanDto CreatePlanDto(Plan record) =>
+         new PlanDto()
+         {
+             Id = record.Id,
+             Title = record.Title,
+             AccountSize = record.AccountSize,
+             CategoryId = record.CategoryId,
+             Commission = record.Commission,
+             CommissionType = record.CommissionType,
+             ConsistencyRule = record.ConsistencyRule,
+             ExpertAdvisorsAvailable = record.ExpertAdvisorsAvailable,
+             FirstPayoutMethod = record.FirstPayoutMethod,
+             MaximumDailyLossPercentage = record.MaximumDailyLossPercentage,
+             MaximumOverallLossPercentage = record.MaximumOverallLossPercentage,
+             MinimumTradingDay = record.MinimumTradingDay,
+             NewsTradingAvailable = record.NewsTradingAvailable,
+             NumberOfPhases = record.NumberOfPhases,
+             PhaseProfitSharePercentage = record.PhaseProfitSharePercentage,
+             ProfitSplitPercentage = record.ProfitSplitPercentage,
+             ResetDiscountPercentage = record.ResetDiscountPercentage,
+             SubsequentPayouts = record.SubsequentPayouts,
+             TradeCopierAvailable = record.TradeCopierAvailable,
+             TradingLeverage = record.TradingLeverage,
+             WeekendHoldingAvailable = record.WeekendHoldingAvailable,
+             Order = record.Order,
+         };
+}
