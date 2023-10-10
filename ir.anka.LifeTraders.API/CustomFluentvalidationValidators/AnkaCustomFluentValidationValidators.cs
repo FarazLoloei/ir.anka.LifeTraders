41b5aae08778823a240cb4691bc7df37d540e749
FarazLoloei <FarazLoloei@users.noreply.github.com>
2023-10-10 16:26:18 +0330

    Plan and Currency fluentvalidation is added

---
+++ ir.anka.LifeTraders.API/CustomFluentvalidationValidators/AnkaCustomFluentValidationValidators.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ir.anka.LifeTraders.WebAPI.CustomFluentvalidationValidators;
+
+public static class AnkaCustomFluentValidationValidators
+{
+    public static IRuleBuilderOptions<T, byte> MustHaveValidPercentageValue<T>(this IRuleBuilder<T, byte> ruleBuilder)
+    {
+        return ruleBuilder.Must(list => list >= 0 && list <= 100).WithMessage("Entered value must between 0, 100");
+    }
+}
+
+
---
+++ ir.anka.LifeTraders.API/DTOs/Validations/Fund/Plan/PlanCreateCommandValidation.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using ir.anka.LifeTraders.Fund.Core.ApplicationService.PlanApplication.Commands;
+
+namespace ir.anka.LifeTraders.WebAPI.DTOs.Validations.Fund.Plan;
+
+public class PlanCreateCommandValidation : AbstractValidator<PlanCreateCommand>
+{
+    public PlanCreateCommandValidation()
+    {
+        RuleFor(x => x.PlanCreateDto).NotNull().SetValidator(new PlanCreateDtoValidation());
+    }
+}
